c7d1ee4d644b87a596de545fc1993e631acd0fde
Nilam Pathak <nilampathak72@gmail.com>
2020-07-05 22:32:39 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Features:
+
+## Setup 01:
+- To install the dependencies and configure them with the app,
+- To add INDEX route and template.
+
+## Setup 02:
+- To add header and footer partials.
+- To include Semantic-UI.
+- To add a menu.
+
+## Setup 03:
+- To add NEW route and template.
+- To add CREATE route to handle post request.
+- To add external stylesheet.
+
+## Setup 04:
+- To add SHOW route and template.
+- To add link to see more about a blog post.
+- To style the SHOW template.
+- To demonstrate use of toDateString() and substring().
+
+## Setup 05:
+- To add Update route and form.
+- To demonstrate the PUT method using method-override. 
+- To use findByIdAndUpdat(id, newData, callback) method for updating the post.
+
+## Setup 06:
+- To enable delete a post.
+- To use findByIdAndRemove(id, callback) method for deleting the post.
---
+++ app.js
@@ -0,0 +1,88 @@
+var express = require("express"),
+    app = express(),
+    methodOverride = require("method-override"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+const port = 3000;
+
+mongoose.connect("mongodb://localhost/restful_blog", { useNewUrlParser: true, useUnifiedTopology: true });
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+var blogSchema = mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "First test blog",
+//     image: "https://cdn.business2community.com/wp-content/uploads/2016/11/Blogging.jpg",
+//     body: "This is a sample post content to test our application."
+// });
+
+app.get("/", (req, res) => res.redirect("/blogs"));
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, items) => {
+        if (err)
+            console.log(err);
+        else
+            res.render("index", { blogs: items });
+    })
+});
+
+app.get("/blogs/new", (req, res) => {
+    res.render("new");
+});
+
+app.post("/blogs", (req, res) => {
+    Blog.create(req.body.blog, (err, item) => {
+        if (err)
+            res.render("new");
+        else
+            res.redirect("/blogs");
+    });
+});
+
+app.get("/blogs/:id", (req, res) => {
+    Blog.findById(req.params.id, (err, item) => {
+        if (err)
+            res.redirect("/blogs");
+        else
+            res.render("show", { blog: item });
+    })
+});
+
+app.get("/blogs/:id/edit", (req, res) => {
+    Blog.findById(req.params.id, (err, item) => {
+        if (err)
+            res.redirect("/blogs");
+        else
+            res.render("edit", { blog: item });
+    });
+});
+
+app.put("/blogs/:id", (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, item) => {
+        if (err)
+            res.redirect("/blogs");
+        else
+            res.redirect("/blogs/" + req.params.id);
+    })
+});
+
+app.delete("/blogs/:id", (req, res) => {
+    Blog.findByIdAndRemove(req.params.id, (err) => {
+        if (err)
+            res.redirect("/blogs");
+        else
+            res.redirect("/blogs");
+    })
+})
+
+app.listen(port, () => console.log(`Server is running at the port ${port}...`));
